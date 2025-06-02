9c37c27de6ac5a68b4b3c0750fc51b8d17d54ef9
Miguel Mata <mmata@altice.com.do>
2025-06-02 17:50:49 -0400

    agregando entidades

---
+++ todo.list.domain/Common/CommonEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace todo.list.domain.Common;
+
+public abstract class CommonEntity
+{
+    public bool IsActive { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public string? UpdatedBy { get; set; }
+    public string? RowGuid { get; set; }
+}
---
+++ todo.list.domain/Entities/TodoItemEntity.cs
@@ -0,0 +1,12 @@
+﻿using todo.list.domain.Common;
+
+namespace todo.list.domain.Entities;
+
+public class TodoItemEntity : CommonEntity
+{
+    public long TodoItemId { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime? CompletedAt { get; set; }
+}
