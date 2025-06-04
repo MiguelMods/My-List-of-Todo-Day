9ac4c223e1173be04a171c34ecd00b8e9703ec4f
Miguel Mata <mmata@altice.com.do>
2025-06-04 10:21:08 -0400

    agregando clase de contexto

---
+++ todo.list.persistance/DataBaseContext/TodoListDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using todo.list.domain.Entities;
+using todo.list.persistance.Configurations;
+
+namespace todo.list.persistance.DataBaseContext;
+
+public class TodoListDBContext(DbContextOptions<TodoListDBContext> dbContextOptions) : DbContext(dbContextOptions)
+{
+    public DbSet<PermissionEntity> permissionEntities { get; set; }
+    public DbSet<RoleEntity> Roles { get; set; }
+    public DbSet<RolePermissionEntity> RolePermissions { get; set; }
+    public DbSet<TodoItemEntity> Todos { get; set; }
+    public DbSet<TodoListItemEntity> TodoLists { get; set; }
+    public DbSet<UserEntity> Users { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new PermissionEntityConfiguration());
+        modelBuilder.ApplyConfiguration(new RoleEntityConfiguration());
+        modelBuilder.ApplyConfiguration(new RolePermissionEntityConfiguration());
+        modelBuilder.ApplyConfiguration(new TodoItemEntityConfiguration());
+        modelBuilder.ApplyConfiguration(new TodoListItemEntityConfiguration());
+        modelBuilder.ApplyConfiguration(new UserEntityConfiguration());
+    }
+}
