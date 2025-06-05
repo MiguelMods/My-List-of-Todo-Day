910f9fc212d1b19adaa7c24a0c74dc2e58df75ae
Miguel Mata <mmata@altice.com.do>
2025-06-05 14:33:46 -0400

    creando repositorio de usuarios con metodos iniciales o unicos

---
+++ todo.list.application/Repositories/Contracts/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using todo.list.domain.Entities;
+
+namespace todo.list.application.Repositories.Contracts;
+
+public interface IUserRepository : IGenericRepository<UserEntity>
+{
+    Task<UserEntity?> GetUserByEmailAsync(string email);
+    Task<UserEntity?> GetUserByEmailAndPasswordAsync(string email, string password);
+    Task<UserEntity?> GetUserByNickNameAsync(string nickName);
+    Task<UserEntity?> GetUserByNickNameAndPasswordAsync(string nickName, string password);
+}
---
+++ todo.list.persistance/Repositories/Implentations/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using todo.list.application.Repositories.Contracts;
+using todo.list.domain.Entities;
+using todo.list.persistance.DataBaseContext;
+
+namespace todo.list.persistance.Repositories.Implentations;
+
+public class UserRepository(TodoListDBContext todoListDBContext) : GenericRepository<UserEntity>(todoListDBContext), IUserRepository
+{
+    public async Task<UserEntity?> GetUserByEmailAsync(string email)
+        => await TodoListDBContext.Users.Where(x => x.Email == email).FirstOrDefaultAsync();
+    public async Task<UserEntity?> GetUserByEmailAndPasswordAsync(string email, string password)
+        => await TodoListDBContext.Users.Where(x => x.Email == email && x.Password == password).FirstOrDefaultAsync();
+    public async Task<UserEntity?> GetUserByNickNameAsync(string nickName)
+        => await TodoListDBContext.Users.Where(x => x.Nickname == nickName).FirstOrDefaultAsync();
+    public async Task<UserEntity?> GetUserByNickNameAndPasswordAsync(string nickName, string password)
+        => await TodoListDBContext.Users.Where(x => x.Nickname == nickName && x.Password == password).FirstOrDefaultAsync();
+}
