bdc93e06d48a30475733d8c11233e28531bd94f8
Miguel Mata <mmata@altice.com.do>
2025-06-05 08:35:30 -0400

    agregando unit of work interfaz y repositorio

---
+++ todo.list.application/Repositories/Contracts/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using todo.list.domain.Common;
+
+namespace todo.list.application.Repositories.Contracts;
+
+public interface IUnitOfWork : IDisposable
+{
+    IGenericRepository<TEntity> GetGenericRepository<TEntity>() where TEntity : CommonEntity;
+    Task<int> SaveChangesAsync();
+    Task<int> SaveChangesWithTransactionAsync();
+    Task RollbackAsync();
+    Task CommitAsync();
+}
---
+++ todo.list.persistance/Repositories/Implentations/UnitOfWork.cs
@@ -0,0 +1,56 @@
+﻿using todo.list.application.Repositories.Contracts;
+using todo.list.domain.Common;
+using todo.list.persistance.DataBaseContext;
+
+namespace todo.list.persistance.Repositories.Implentations;
+
+public class UnitOfWork(TodoListDBContext todoListDBContext) : IUnitOfWork
+{
+    private TodoListDBContext TodoListDBContext { get; } = todoListDBContext;
+
+    public IGenericRepository<TEntity> GetGenericRepository<TEntity>() where TEntity : CommonEntity => new GenericRepository<TEntity>(TodoListDBContext);
+
+    public async Task<int> SaveChangesAsync()
+    {
+        var result = await TodoListDBContext.SaveChangesAsync();
+        return result;
+    }
+
+    public async Task<int> SaveChangesWithTransactionAsync()
+    {
+        await using var transaction = await TodoListDBContext.Database.BeginTransactionAsync();
+        try
+        {
+            var result = await TodoListDBContext.SaveChangesAsync();
+            await transaction.CommitAsync();
+            return result;
+        }
+        catch
+        {
+            await transaction.RollbackAsync();
+            throw;
+        }
+    }
+
+    public async Task CommitAsync()
+    {
+        if (TodoListDBContext.Database.CurrentTransaction != null)
+        {
+            await TodoListDBContext.Database.CommitTransactionAsync();
+        }
+    }
+
+    public async Task RollbackAsync()
+    {
+        if (TodoListDBContext.Database.CurrentTransaction != null)
+        {
+            await TodoListDBContext.Database.RollbackTransactionAsync();
+        }
+    }
+
+    public async void Dispose()
+    {
+        await TodoListDBContext.DisposeAsync();
+        GC.SuppressFinalize(this); // Fix for CA1816
+    }
+}
