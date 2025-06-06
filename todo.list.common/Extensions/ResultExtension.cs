9da9ab83070df6844b655389ede26caddac4ed87
Miguel Mata <mmata@altice.com.do>
2025-06-06 14:42:08 -0400

    agregando clase de resultados

---
+++ todo.list.common/Extensions/ResultExtension.cs
@@ -0,0 +1,13 @@
+﻿using todo.list.common.Results;
+
+namespace todo.list.common.Extensions;
+
+public static class ResultExtension
+{
+    public static Result<Type> IsASuccess<Type>(this Type? data, string? message = null)
+        => Result<Type>.Success(data, message);
+    public static Result<Type> IsAFailure<Type>(this string? message)
+        => Result<Type>.Failure(message);
+    public static Result<Type> IsAFailure<Type>(this Type? data, string? message = null)
+        => Result<Type>.Failure(data, message);
+}
---
+++ todo.list.common/Results/Result.cs
@@ -0,0 +1,32 @@
+﻿namespace todo.list.common.Results;
+
+public class Result<Type>
+{
+    public bool IsSuccess { get; set; }
+    public Type? Data { get; set; }
+    public string? Message { get; set; }
+    public static Result<Type> Success() => new()
+    {
+        IsSuccess = true,
+        Data = default,
+        Message = null
+    };
+    public static Result<Type> Success(Type? data, string? message = null) => new()
+    {
+        IsSuccess = true,
+        Data = data,
+        Message = message
+    };
+    public static Result<Type> Failure(string? message) => new()
+    {
+        IsSuccess = false,
+        Data = default,
+        Message = message
+    };
+    public static Result<Type> Failure(Type? data, string? message) => new()
+    {
+        IsSuccess = false,
+        Data = data,
+        Message = message
+    };
+}
